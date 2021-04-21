c54c8c183a30f4fe0d4c37e015e3fbd86a773d9a
grepwood@sucs.org <grepwood@sucs.org>
2021-04-21 05:22:40 +0200

    Reported by user that re and json are integrated

--- generate_requirements.sh
+++ generate_requirements.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-
-INTEGRATED_LIBS='sys|subprocess|winreg|pdb'
-
-OWN_LIBS='mirrors|util'
-
-grep import . -r | grep -vE '\.log|#|LICENSE|\.git' | grep \\.py: | sed 's/^.*://;s/\t*//;s/\ import.*$//;s/^from\ /import\ /;s/\..*$//' | grep -vE "${INTEGRATED_LIBS}|${OWN_LIBS}" | sort -u | sed 's/mega/mega\.py/;s/import\ //' > requirements.txt
+INTEGRATED_LIBS='^sys$|^subprocess$|^winreg$|^pdb$|^json$|^re$|^traceback$'
+OWN_LIBS='^mirrors$|^util$|^shinden$|^browser$'
+grep import $(find . -type f -name '*.py') | sed 's/^.*://;s/\t*//;s/\ import.*$//;s/^from\ /import\ /;s/\..*$//;s/^import\ //' | grep -vE "${INTEGRATED_LIBS}|${OWN_LIBS}" | sort -u > requirements.txt
